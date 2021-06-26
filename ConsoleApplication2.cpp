4bf8fa686dfd97aaae510b59a27ac52be55c8cb4
antonshlenko <84861432+antonshlenko@users.noreply.github.com>
2021-06-26 18:50:28 +0400

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double x, y, z;
+
+    double* px, * py, ** prx, ** pry;
+
+    cout << "Введите первое число: " << endl;
+
+    cin >> x;
+
+    cout << "Введите второе число: " << endl;
+
+    cin >> y;
+
+    px = &x;
+    py = &y;
+
+    prx = &px;
+    pry = &py;
+
+    z = **prx + **pry;
+
+    cout << "Сумма: " << z << endl;
+}
+
+
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+
+    double x, y, * px, * py;
+
+    char key, * pkey;
+
+    cout << "Введите первое число: " << endl;
+
+    cin >> x;
+
+    cout << "Введите второе число: " << endl;
+
+    cin >> y;
+
+    px = &x;
+    py = &y;
+
+    cout << "Выберите действие '+' '-' '*' '/' " << endl;
+
+    cin >> key;
+
+    pkey = &key;
+
+    switch (*pkey)
+    {
+    case '+':
+        cout << *px << " + " << *py << " = " << *px + *py << endl;
+        break;
+    case '-':
+        cout << *px << " - " << *py << " = " << *px - *py << endl;
+        break;
+    case '*':
+        cout << *px << " * " << *py << " = " << (*px) * (*py) << endl;
+        break;
+    case '/':
+        cout << *px << " / " << *py << " = " << (*px) / (*py) << endl;
+        break;
+
+    default:
+        cout << "Введенный Вами символ некорректен " << endl;
+        break;
+    }
+
+}
+
+
+
+
+
+
